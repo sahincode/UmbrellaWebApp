98c05eca8bd632fb41f67fdff298e66c9627dc7b
sahincode <ismayilovsahin714@gmail.com>
2024-11-30 14:17:11 +0400

    Catalog.API DeleteProduct method created successfully.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id):ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async  Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handler is called by {@command}", command);
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if(product is null)
+            {
+                throw new ProductNotFoundException(command.Id);
+            }
+             session.Delete<Product>(product.Id);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+            
+        }
+    }
+}
